fbf4ba140b3e8ea8a8fdff9ed9fb7eb8f16904d1
MrZhaozhirong <2377871036@qq.com>
2018-10-29 17:04:20 +0800

    FMOD下

---
+++ app/src/main/java/org/zzrblog/fmod/EffectActivity.java
@@ -0,0 +1,60 @@
+package org.zzrblog.fmod;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import org.zzrblog.blogapp.R;
+
+import java.io.File;
+
+/**
+ * Created by zzr on 2018/10/29.
+ */
+
+public class EffectActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fmod_effect);
+        org.fmod.FMOD.init(this);
+        VoiceEffectUtils.init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VoiceEffectUtils.release();
+        org.fmod.FMOD.close();
+    }
+
+    public void onClickedEffect(View btn){
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.pathSeparator + "singing.wav";
+        //String path = "file:///android_asset/"+"dfb.mp3";
+        switch (btn.getId()) {
+            case R.id.btn_normal:
+                VoiceEffectUtils.play(VoiceEffectUtils.MODE_NORMAL);
+                break;
+            case R.id.btn_luoli:
+                VoiceEffectUtils.play(VoiceEffectUtils.MODE_LUOLI);
+                break;
+            case R.id.btn_dashu:
+                VoiceEffectUtils.play(VoiceEffectUtils.MODE_DASHU);
+                break;
+            case R.id.btn_jingsong:
+                VoiceEffectUtils.play(VoiceEffectUtils.MODE_JINGSONG);
+                break;
+            case R.id.btn_gaoguai:
+                VoiceEffectUtils.play(VoiceEffectUtils.MODE_GAOGUAI);
+                break;
+            case R.id.btn_kongling:
+                VoiceEffectUtils.play(VoiceEffectUtils.MODE_KONGLING);
+                break;
+            default:
+                break;
+        }
+    }
+}
--- app/src/main/java/org/zzrblog/fmod/VoiceEffectUtils.java
+++ app/src/main/java/org/zzrblog/fmod/VoiceEffectUtils.java
@@ -6,13 +6,22 @@ package org.zzrblog.fmod;
 
 public class VoiceEffectUtils {
 
+    //音效的类型
+    public static final int MODE_NORMAL = 0;
+    public static final int MODE_LUOLI = 1;
+    public static final int MODE_DASHU = 2;
+    public static final int MODE_JINGSONG = 3;
+    public static final int MODE_GAOGUAI = 4;
+    public static final int MODE_KONGLING = 5;
 
-    private native void play(String path, int type);
+    public static native void init();
 
+    public static native void play(int type);
+
+    public static native void release();
 
     static
     {
-
         // Try logging libraries...
         try { System.loadLibrary("fmodL");
         } catch (UnsatisfiedLinkError e) { }
@@ -20,7 +29,6 @@ public class VoiceEffectUtils {
         try { System.loadLibrary("fmod");
         } catch (UnsatisfiedLinkError e) { }
 
-        //System.loadLibrary("stlport_shared");
-        System.loadLibrary("fmod-voice-lib");
+        //System.loadLibrary("编写的库名字");
     }
 }
