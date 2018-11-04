efcf00b19a2fbcf4b4b5318794a85e51e0247c9f
zzr <2377871036@qq.com>
2018-11-04 18:02:51 +0800

    FMOD下

--- app/src/main/cpp/fmod/effect_sound.cpp
+++ app/src/main/cpp/fmod/effect_sound.cpp
@@ -44,6 +44,8 @@ void Java_org_zzrblog_fmod_VoiceEffectUtils_play(JNIEnv *env, jclass clazz, jint
     FMOD::Sound     *sound;
     FMOD::Channel   *channel;
     bool            playing = true; //判断音频是否还在播放.
+    FMOD::DSP       *dsp;
+
 
     //初始化 FMOD.System
     System_Create(&system);
@@ -58,10 +60,51 @@ void Java_org_zzrblog_fmod_VoiceEffectUtils_play(JNIEnv *env, jclass clazz, jint
     try {
         switch (type)
         {
-            case MODE_NORMAL: //触发音频播放
+            case MODE_NORMAL:{
+                //触发音频播放
                 system->playSound(sound, 0, false, &channel);
-                break;
-            // ...
+            }break;
+            case MODE_LUOLI:{
+                //DSP digital signal process
+                //FMOD_DSP_TYPE_PITCHSHIFT dsp，fmod中预定义好的音效，提升或者降低音调用的一种音效
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp);
+                //设置音调的参数
+                dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,2.5);
+                //触发音频播放
+                system->playSound(sound, 0, false, &channel);
+                //添加到channel
+                channel->addDSP(0,dsp);
+            }break;
+            case MODE_JINGSONG:{
+                //惊悚
+                system->createDSPByType(FMOD_DSP_TYPE_TREMOLO,&dsp);
+                dsp->setParameterFloat(FMOD_DSP_TREMOLO_SKEW, 0.5);
+                system->playSound(sound, 0, false, &channel);
+                channel->addDSP(0,dsp);
+            }break;
+            case MODE_DASHU:{
+                //大叔
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&dsp);
+                dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,0.8);
+                system->playSound(sound, 0, false, &channel);
+                channel->addDSP(0,dsp);
+            }break;
+            case MODE_GAOGUAI:{
+                //搞怪 提高说话的速度
+                float frequency = 0;
+                system->playSound(sound, 0, false, &channel);
+                channel->getFrequency(&frequency);
+                frequency = frequency * 1.6f;
+                channel->setFrequency(frequency);
+            }break;
+            case MODE_KONGLING:{
+                //空灵
+                system->createDSPByType(FMOD_DSP_TYPE_ECHO,&dsp);
+                dsp->setParameterFloat(FMOD_DSP_ECHO_DELAY,300);
+                dsp->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK,20);
+                system->playSound(sound, 0, false, &channel);
+                channel->addDSP(0,dsp);
+            }break;
             default:
                 break;
         }
