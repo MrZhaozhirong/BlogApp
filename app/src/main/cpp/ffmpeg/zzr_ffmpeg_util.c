d4bcda19948102752ca9aa6eabe6936948f391a3
MrZhaozhirong <2377871036@qq.com>
2018-12-05 15:42:22 +0800

    多进程-ffmpeg-解码

--- app/src/main/cpp/ffmpeg/zzr_ffmpeg_util.c
+++ app/src/main/cpp/ffmpeg/zzr_ffmpeg_util.c
@@ -21,6 +21,7 @@ Java_org_zzrblog_mp_ZzrFFmpeg_Mp4TOYuv(JNIEnv *env, jclass clazz, jstring input_
 
     // 1.注册组件
     av_register_all();
+    avformat_network_init();
     // 2.获取格式上下文指针，便于打开媒体容器文件获取媒体信息
     AVFormatContext *pFormatContext = avformat_alloc_context();
     // 打开输入视频文件
@@ -57,19 +58,24 @@ Java_org_zzrblog_mp_ZzrFFmpeg_Mp4TOYuv(JNIEnv *env, jclass clazz, jstring input_
         LOGE("%s","创建解码器对应的上下文失败.");
         return -4;
     }
+    pCodecContext->width = 1920;
+    pCodecContext->height = 1080;
+    pCodecContext->pix_fmt = AV_PIX_FMT_YUV420P;
     // 打开解码器
     if(avcodec_open2(pCodecContext, pCodec, NULL) < 0){
         LOGE("%s","解码器无法打开");
         return -5;
     } else {
-        LOGI("当前解码器pix_fmt：%d", pCodecContext->pix_fmt);
+        LOGI("设置解码器解码格式pix_fmt：%d", pCodecContext->pix_fmt);
     }
 
 
     // 4.解码准备，多看多理解。
 
-    // 编码数据对象
-    AVPacket *packet = (AVPacket *)av_malloc(sizeof(AVPacket));
+    // 解压缩数据对象
+    //AVPacket *packet = (AVPacket *)av_malloc(sizeof(AVPacket));
+    //av_init_packet(packet);
+    AVPacket *packet = av_packet_alloc();
     // 解码数据对象
     AVFrame *frame = av_frame_alloc();
     AVFrame *yuvFrame = av_frame_alloc();
@@ -81,8 +87,12 @@ Java_org_zzrblog_mp_ZzrFFmpeg_Mp4TOYuv(JNIEnv *env, jclass clazz, jstring input_
     av_image_fill_arrays(yuvFrame->data, yuvFrame->linesize, out_buffer,
                          AV_PIX_FMT_YUV420P, pCodecContext->width, pCodecContext->height, 1 );
 
-    // 打开输出文件
+    // yuv输出文件
     FILE* fp_yuv = fopen(output_path_cstr,"wb");
+    // test：264输出文件
+    char save264str[100]={0};
+    sprintf(save264str,"%s", "/storage/emulated/0/10s_test.h264");
+    FILE* fp_264 = fopen(save264str,"wb");
 
     //用于像素格式转换或者缩放
     struct SwsContext *sws_ctx = sws_getContext(
@@ -91,13 +101,16 @@ Java_org_zzrblog_mp_ZzrFFmpeg_Mp4TOYuv(JNIEnv *env, jclass clazz, jstring input_
             SWS_BICUBIC, NULL, NULL, NULL); //SWS_BILINEAR
 
     int ret, isGot, frameCount = 0;
-    // 5. 循环读取压缩的视频数据 AVPacket
+    // 5. 循环读取视频数据的分包 AVPacket
     while(av_read_frame(pFormatContext, packet) >= 0){
-        if(packet->stream_index == video_stream_idx) {
-            //解码AVPacket->AVFrame
+        if(packet->stream_index == video_stream_idx)
+        {
+            // test：h264数据写入本地文件
+            fwrite(packet->data, 1, (size_t) packet->size, fp_264);
+            //AVPacket->AVFrame
             ret = avcodec_decode_video2(pCodecContext, frame, &isGot, packet);
             if(ret < 0){
-                LOGE("解码失败.\n");
+                LOGE("解码失败：%d\n", ret);
             }
             //Zero if no frame could be decompressed
             if(isGot){
@@ -109,7 +122,7 @@ Java_org_zzrblog_mp_ZzrFFmpeg_Mp4TOYuv(JNIEnv *env, jclass clazz, jstring input_
                 //向YUV文件保存解码之后的帧数据
                 //AVFrame->YUV
                 //一个像素包含一个Y
-                int y_size = pCodecContext->width * pCodecContext->height;
+                int y_size = frame->width * frame->height;
                 fwrite(yuvFrame->data[0], 1, (size_t) y_size, fp_yuv);
                 fwrite(yuvFrame->data[1], 1, (size_t) y_size/4, fp_yuv);
                 fwrite(yuvFrame->data[2], 1, (size_t) y_size/4, fp_yuv);
@@ -137,7 +150,7 @@ Java_org_zzrblog_mp_ZzrFFmpeg_Mp4TOYuv(JNIEnv *env, jclass clazz, jstring input_
     }
     LOGI("总共解码%d帧",frameCount++);
     fclose(fp_yuv);
-
+    fclose(fp_264);
 
     // 结束回收工作
     sws_freeContext(sws_ctx);
