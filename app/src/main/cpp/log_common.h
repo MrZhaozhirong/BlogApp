5c1828bd75136e0ec86a41087d570e1891be02b8
MrZhaozhirong <2377871036@qq.com>
2018-12-10 10:52:00 +0800

    日志输出

--- app/src/main/cpp/log_common.h
+++ app/src/main/cpp/log_common.h
@@ -1,15 +1,43 @@
 //
 // Created by nicky on 2018/11/29.
 //
+#ifndef LOG_COMMON_H
+#define LOG_COMMON_H
+
 #include <android/log.h>
+#include <string.h>
 
-#ifndef BLOGAPP_ZZR_BLOG_COMMON_H
-#define BLOGAPP_ZZR_BLOG_COMMON_H
+#define DEBUG
 
-#define LOGD(FORMAT,...) __android_log_print(ANDROID_LOG_DEBUG,"ZzrBlogApp",FORMAT,##__VA_ARGS__);
-#define LOGI(FORMAT,...) __android_log_print(ANDROID_LOG_INFO,"ZzrBlogApp",FORMAT,##__VA_ARGS__);
-#define LOGW(FORMAT,...) __android_log_print(ANDROID_LOG_WARN,"ZzrBlogApp",FORMAT,##__VA_ARGS__);
-#define LOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR,"ZzrBlogApp",FORMAT,##__VA_ARGS__);
+// Windows 和 Linux 这两个宏是在 CMakeLists.txt 通过 ADD_DEFINITIONS 定义的
+#ifdef Windows
+#define __FILENAME__ (strrchr(__FILE__, '\\') + 1) // Windows下文件目录层级是'\\'
+#elif Linux
+#define __FILENAME__ (strrchr(__FILE__, '/') + 1) // Linux下文件目录层级是'/'
+#else
+#define __FILENAME__ (strrchr(__FILE__, '/') + 1) // 默认使用这种方式
+#endif
 
 
-#endif //BLOGAPP_ZZR_BLOG_COMMON_H
+#ifdef DEBUG
+#define TAG "JNI"
+#define LOGV(format, ...) __android_log_print(ANDROID_LOG_VERBOSE, TAG,\
+        "[%s][%s][%d]: " format, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#define LOGD(format, ...) __android_log_print(ANDROID_LOG_DEBUG, TAG,\
+        "[%s][%s][%d]: " format, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#define LOGI(format, ...) __android_log_print(ANDROID_LOG_INFO, TAG,\
+        "[%s][%s][%d]: " format, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#define LOGW(format, ...) __android_log_print(ANDROID_LOG_WARN, TAG,\
+        "[%s][%s][%d]: " format, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#define LOGE(format, ...) __android_log_print(ANDROID_LOG_ERROR, TAG,\
+        "[%s][%s][%d]: " format, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#define LOGV(format, ...)  ;
+#define LOGD(format, ...)  ;
+#define LOGI(format, ...)  ;
+#define LOGW(format, ...)  ;
+#define LOGE(format, ...)  ;
+#endif // DEBUG
+
+
+#endif //LOG_COMMON_H
