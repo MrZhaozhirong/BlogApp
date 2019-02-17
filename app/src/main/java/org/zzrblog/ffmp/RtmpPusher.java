9f213bc3bc79cf0b98a4f43beca9d8c43b453986
zzr <2377871036@qq.com>
2019-02-17 12:06:41 +0800

    原生音视频编码

--- app/src/main/java/org/zzrblog/ffmp/RtmpPusher.java
+++ app/src/main/java/org/zzrblog/ffmp/RtmpPusher.java
@@ -6,6 +6,43 @@ package org.zzrblog.ffmp;
 
 public class RtmpPusher {
 
+    static {
+        System.loadLibrary("rtmp_push");
+    }
 
+    public native void startPush(String url);
+
+    public native void stopPush();
+
+    public native void release();
+
+    /**
+     * 设置视频参数
+     * @param width
+     * @param height
+     * @param bitrate
+     * @param fps
+     */
+    public native void setVideoOptions(int width, int height, int bitrate, int fps);
+
+    /**
+     * 设置音频参数
+     * @param sampleRateInHz
+     * @param channel
+     */
+    public native void setAudioOptions(int sampleRateInHz, int channel);
+
+    /**
+     * 发送视频数据
+     * @param data nv21数据
+     */
+    public native void feedVideoData(byte[] data);
+
+    /**
+     * 发送音频数据
+     * @param data pcm数据
+     * @param len 数据长度
+     */
+    public native void feedAudioData(byte[] data, int len);
 
 }
