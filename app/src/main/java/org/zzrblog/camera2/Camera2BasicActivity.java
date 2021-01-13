91671d5ba9699643dc23b7a6334ea1e0d6d1b71f
ZhaoZhiRong <2377871036@qq.com>
2021-01-13 11:40:11 +0800

    Add PreviewImageReader，回传yuv_420_888的数据。

--- app/src/main/java/org/zzrblog/camera2/Camera2BasicActivity.java
+++ app/src/main/java/org/zzrblog/camera2/Camera2BasicActivity.java
@@ -7,6 +7,9 @@ import org.zzrblog.blogapp.R;
 
 /**
  * Created by zzr on 2021/1/12.
+ * https://developer.android.google.cn/training/camera/multiple-camera-streams-simultaneously
+ * https://www.jianshu.com/p/23e8789fbc10
+ * https://mp.weixin.qq.com/s/hbls80hCCftnFLkO8U5Kbw
  */
 
 public class Camera2BasicActivity extends AppCompatActivity {
